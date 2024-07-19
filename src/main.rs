0ba90b480288e0f788d152237b525664ec61c3db
Mustafa Moiz <mustafa.moiz125@gmail.com>
2024-07-19 16:47:58 -0700

    feat: emit metric timestamp

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::{collections::HashMap, time::SystemTime};
+
+use chrono::{DateTime, Utc};
+
 fn main() {
-    println!("Hello, world!");
+    metric(
+        "requests",
+        1,
+        HashMap::from([("user".into(), "bob".into())]),
+    );
+}
+
+fn metric(name: &str, value: u64, dimensions: HashMap<String, String>) {
+    let timestamp = {
+        let dt = SystemTime::now();
+        let dt: DateTime<Utc> = dt.into();
+        dt.to_rfc3339()
+    };
+
+    let mut message = format!("emitting metric ({timestamp}): {name} = {value}");
+
+    let dimensions = dimensions
+        .iter()
+        .map(|(key, val)| format!("{key}: {val}"))
+        .collect::<Vec<_>>()
+        .join(", ");
+
+    message.push_str(&format!(" {{ {dimensions} }}"));
+
+    println!("{message}");
 }
