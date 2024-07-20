f84d93a9757ec2ed8967759d81ac059ee8ea7e8d
Mustafa S. Moiz <msmoiz@amazon.com>
2024-07-19 17:32:11 -0700

    feat: update metric log format
    The new format is closer to the format used by Prometheus.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use std::fmt::Write;
 use std::{collections::HashMap, time::SystemTime};
 
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, SecondsFormat, Utc};
 
 fn main() {
     metric(
@@ -14,10 +15,10 @@ fn metric(name: &str, value: u64, dimensions: HashMap<String, String>) {
     let timestamp = {
         let dt = SystemTime::now();
         let dt: DateTime<Utc> = dt.into();
-        dt.to_rfc3339()
+        dt.to_rfc3339_opts(SecondsFormat::Secs, false)
     };
 
-    let mut message = format!("emitting metric ({timestamp}): {name} = {value}");
+    let mut message = format!("{name}");
 
     let dimensions = dimensions
         .iter()
@@ -25,7 +26,9 @@ fn metric(name: &str, value: u64, dimensions: HashMap<String, String>) {
         .collect::<Vec<_>>()
         .join(", ");
 
-    message.push_str(&format!(" {{ {dimensions} }}"));
+    write!(&mut message, "{{ {dimensions} }}").unwrap();
+
+    write!(&mut message, " {value} @ {timestamp}").unwrap();
 
     println!("{message}");
 }
